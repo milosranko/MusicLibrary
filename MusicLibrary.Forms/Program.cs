7428ba41cc8991c6b6e51abfb206e83b162a572f
Milos Rankovic <milos.ranko@gmail.com>
2021-07-29 22:14:53 +0200

    Updated ffmpeg libraries, added zip file for ffmpeg

--- MusicLibrary.Forms/Program.cs
+++ MusicLibrary.Forms/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.IO.Compression;
 using System.Windows.Forms;
 
 namespace MusicLibrary.Forms
@@ -11,10 +13,24 @@ namespace MusicLibrary.Forms
         [STAThread]
         static void Main()
         {
+            PrepareFFMpegFiles();
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void PrepareFFMpegFiles()
+        {
+            var path = $"{Environment.CurrentDirectory}\\ffmpeg\\ffmpeg.zip";
+
+            using var archive = ZipFile.OpenRead(path);
+            foreach (var entry in archive.Entries)
+            {
+                var destinationPath = Path.GetFullPath(Path.Combine($"{Environment.CurrentDirectory}\\ffmpeg", entry.FullName));
+                entry.ExtractToFile(destinationPath, true);
+            }
         }
     }
 }
