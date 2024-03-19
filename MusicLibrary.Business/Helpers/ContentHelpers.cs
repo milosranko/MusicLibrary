bda850fedb2f856f5a2de0caf55344f200ccd8a8
Miloš Ranković <milos.ranko@live.com>
2024-03-19 16:13:17 +0100

    Removed unused code, set classes to internal

--- MusicLibrary.Business/Helpers/ContentHelpers.cs
+++ MusicLibrary.Business/Helpers/ContentHelpers.cs
@@ -3,23 +3,23 @@ using System.Text.RegularExpressions;
 
 namespace MusicLibrary.Business.Helpers;
 
-public static class ContentHelpers
+internal static class ContentHelpers
 {
-	private static readonly Regex CleanSpacesRegex = new Regex("[\\s\\r\\n]+", RegexOptions.Compiled);
-	private static readonly Regex RemoveSpecialCharactersRegex = new Regex("[^a-zA-Z0-9]", RegexOptions.Compiled);
+    private static readonly Regex CleanSpacesRegex = new Regex("[\\s\\r\\n]+", RegexOptions.Compiled);
+    private static readonly Regex RemoveSpecialCharactersRegex = new Regex("[^a-zA-Z0-9]", RegexOptions.Compiled);
 
-	public static string CleanContent(StringBuilder body)
-	{
-		return CleanSpacesRegex
-			.Replace(RemoveSpecialCharactersRegex.Replace(body.ToString(), " ").ToString(), " ")
-			.Trim()
-			.ToLower();
-	}
+    public static string CleanContent(StringBuilder body)
+    {
+        return CleanSpacesRegex
+            .Replace(RemoveSpecialCharactersRegex.Replace(body.ToString(), " ").ToString(), " ")
+            .Trim()
+            .ToLower();
+    }
 
-	public static string RemoveSpecialCharacters(this string value)
-	{
-		if (string.IsNullOrEmpty(value)) return value;
+    public static string RemoveSpecialCharacters(this string value)
+    {
+        if (string.IsNullOrEmpty(value)) return value;
 
-		return RemoveSpecialCharactersRegex.Replace(value, " ");
-	}
+        return RemoveSpecialCharactersRegex.Replace(value, " ");
+    }
 }
